016914f66163fb6a474d1675a6d9a52bb4d8e89d
Francisco Hernandez <francisco.heral02@gmail.com>
2022-05-09 16:53:06 -0500

    cambio nombre de clase aplicacion

--- src/main/java/com/TemplateNTT/BusinessPartnerNttApplication.java
+++ src/main/java/com/TemplateNTT/BusinessPartnerNttApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemplateNttApplication {
+public class BusinessPartnerNttApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TemplateNttApplication.class, args);
+		SpringApplication.run(BusinessPartnerNttApplication.class, args);
 	}
 
 }
